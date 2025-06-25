a60e47145511f72ca0db4bc40b4172741f6ffbdc
Franco Bustios <166667809+fbustios@users.noreply.github.com>
2025-06-24 20:01:18 -0600

    Notas final

--- src/components/gradesStudent.jsx
+++ src/components/gradesStudent.jsx
@@ -3,10 +3,10 @@ import {useState, useEffect} from "react";
 
 
 function GradesStudent() {
-    //const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('user'));
     const token = localStorage.getItem('token');
     const id = localStorage.getItem('id')
-    //const isProfessor = user.rol === 'Profesor';
+    const isProfessor = user.rol === 'Profesor';
     const [evaluaciones,setEvaluaciones] = useState([])
     const claseId = localStorage.getItem('clase_id')
     const semestreId = localStorage.getItem('semestre');
@@ -32,6 +32,39 @@ function GradesStudent() {
         }
         getEvaluaciones();
     },[id,claseId,semestreId,token]);
+
+    const handleEdit = async (e, notaId) => {
+        let notaNueva = e.target.value;
+
+        if (notaNueva === '') {
+
+            setEvaluaciones(prev =>
+                prev.map(ev => ev.id === notaId ? { ...ev, nota: '' } : ev)
+            );
+            return;
+        }
+            setEvaluaciones(prev =>
+                prev.map(ev => ev.id === notaId ? {...ev, nota: notaNueva} : ev)
+            );
+            try {
+                const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/Home/Dashboard/Clase/Notas/NotasPersonales/Update`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'authorization': 'Bearer ' + token
+                    },
+                    body: JSON.stringify({notaNueva, notaId})
+                })
+                if (!res.ok) throw new Error();
+            } catch (err) {
+                alert('Error al guardar la nota');
+            }
+
+    }
+    let total = 0;
+    for(const e of evaluaciones){
+        total+=e.nota * (e.porcentaje/100)
+    }
     return (
         <div className="notas-wrapper">
             <table className="notas-tabla">
@@ -46,13 +79,23 @@ function GradesStudent() {
                 {evaluaciones.map((evalu, index) => (
                     <tr key={index}>
                         <td>{evalu.nombre}</td>
-                        <td>{evalu.nota}</td>
+                        <td>
+                            {isProfessor ? (
+                            <input
+                                className='btn-pequeno'
+                                type="number"
+                                value={evalu.nota}
+                                onChange={(e) => {handleEdit(e,evalu.id)}}
+                            />) :
+                                (<span>{evalu.nota}</span>)}
+                        </td>
+
                         <td>{evalu.porcentaje}</td>
                     </tr>
                 ))}
                 <tr className="fila-total">
                     <td>Total</td>
-                    <td>- %</td>
+                    <td>{Math.round(total)}</td>
                     <td>100%</td>
                 </tr>
                 </tbody>
